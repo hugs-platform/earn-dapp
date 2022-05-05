915d0c0eab45f3d3b6d61c74b7ecfd7f2cea37fb
Matisse <matisse.van.steenbergen@student.howest.be>
2022-05-06 00:24:35 +0200

    adding typescript, nextJS and prisma template

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+import React from 'react'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ utils/what.ts
@@ -0,0 +1,1 @@
+// for all returning code, functions
