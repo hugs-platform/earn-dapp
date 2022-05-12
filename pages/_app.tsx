7a601020d77ecfaf755ebe079dffd828fbf97b55
Alexander Lohaus <alexander.lohaus@gmail.com>
2022-05-12 23:23:54 +0200

    feat[ED-3]: add wallet logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,34 @@ import "../public/static/styles/reset.css";
 import "../public/static/styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
+import dynamic from "next/dynamic";
 
-// eslint-disable-next-line require-jsdoc
+// const SOLANA_NETWORK = WalletAdapterNetwork.Mainnet;
+const SOLANA_NETWORK = WalletAdapterNetwork.Devnet;
+const network = SOLANA_NETWORK;
+
+// set custom RPC server endpoint for the final website
+const endpoint = process.env.NEXT_PUBLIC_ENDPOINT ? process.env.NEXT_PUBLIC_ENDPOINT : "http://127.0.0.1:8899";
+
+const WalletProvider = dynamic(
+    () => import("../contexts/ClientWalletProvider"),
+    {
+        ssr: false,
+    }
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    //const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider>
+                <Component {...pageProps} />
+            </WalletProvider>
+        </ConnectionProvider>
+    );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from "next";
 import React from "react";
 import styles from "./homePage.module.css";
-import LogIn from "../components/core/solanaLogIn";
+import SolanaLogin from "../components/core/solanaLogIn";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.index_full}>
-      <LogIn />
+      <SolanaLogin />
     </div>
   );
 };
