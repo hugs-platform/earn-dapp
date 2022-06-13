46e56ab480c9fdbde655c85d2cf2295aed5f80a1
Matisse <matisse.van.steenbergen@student.howest.be>
2022-06-13 11:45:04 +0200

    making better APY data

--- data/ApyData.tsx
+++ data/ApyData.tsx
@@ -2,13 +2,16 @@ export const coinInfo = [
   {
     id: 23423423454359234,
     name: "bitcoin",
+    percentage: 23.84,
   },
   {
     id: 23423429455359334,
     name: "ethereum",
+    percentage: 14.19,
   },
   {
     id: 13423423454359235,
     name: "solana",
+    percentage: 38.29,
   },
 ];
