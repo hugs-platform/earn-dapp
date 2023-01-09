19162c3b9c82d031bc08fb852167ef90cff5345f
Mykyta Demianenko <mykytademianenko@gmail.com>
2023-01-09 11:36:07 +0200

    [ED-53] Fix env

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+		NEXT_PUBLIC_SCAN_URL: process.env.NEXT_PUBLIC_SCAN_URL,
+		NEXT_PUBLIC_WALLET_PRIVATE_KEY: process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY,
+	},
   reactStrictMode: true,
   images: {
     domains: ['assets.coingecko.com']
   },
-  env: {
-		NEXT_PUBLIC_SCAN_URL: process.env.NEXT_PUBLIC_SCAN_URL,
-		NEXT_PUBLIC_WALLET_PRIVATE_KEY: process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY,
-	}
 }
 
 module.exports = nextConfig
