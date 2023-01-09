a386f93f4f0a7b48e292e975e791a677dde29502
nickreznichuk <nickreznichuk@gmail.com>
2023-01-09 14:06:45 +0200

    [+] remove toString for apy

--- services/web3/controller.ts
+++ services/web3/controller.ts
@@ -3,7 +3,7 @@ import {contract, web3} from "./core";
 export const saveContributionToContract = async (contribution: any) => {
     const account: any = await web3.eth.accounts.privateKeyToAccount('0xb36a29c85f4e6d42e8cd3f3bcad061f287e96df807d47b61c996bf29424eb10d');
 
-    const tx = await contract.methods.addContribution(contribution.id, contribution.contributor, contribution.market_coin, contribution.apy.toString(), contribution.locked, )
+    const tx = await contract.methods.addContribution(contribution.id, contribution.contributor, contribution.market_coin, contribution.apy, contribution.locked, )
     const gas = await tx.estimateGas({from: account.address})
         const options = {
             to      : tx._parent._address,
