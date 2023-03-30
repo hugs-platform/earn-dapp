97cbfaaa4fa593905817b5e542e5c53bbdacbda3
Mykyta Demianenko <mykytademianenko@gmail.com>
2023-03-30 22:48:33 +0300

    Update sidebar styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,8 @@ const Home: NextPage<HomePageProps> = () => {
   const [ userName, setUserName ] = useState("");
   const [ userAvatar, setUserAvater ] = useState("");
   const [ currentPage, setCurrentPage ] = useState("Coins");
+  const [ fullSidebar, setFullSidebar ] = useState(true);
+  const [ giftShow, setGiftShow ] = useState(true);
   const API = new HugsApi();
 
 
@@ -104,74 +106,134 @@ const Home: NextPage<HomePageProps> = () => {
     }
   });
 
+  const ShowLess = () => {
+    setFullSidebar(false);
+    setGiftShow(false);
+  }
+
+  const ShowMore = () => {
+    setFullSidebar(true);
+    setTimeout(() => {
+      setGiftShow(true);
+    }, 500);
+  }
+
   return (
     <div className={styles.index_full}>
       <Favicon url="/favicon.ico"/>
       <title>(Alpha) Earn Markets</title>
       <script type="module" src={process.env.NEXT_PUBLIC_HUGBUNTERS_WIDGET_URL}/>
       <link href="https://use.fontawesome.com/releases/v6.4.0/css/all.css" rel="stylesheet"/>
-      <ChakraProvider>
-        <Layout>
-          <Navbar>
-            <Container>
-              <Nav className={styles.hugsSideNavBar}>
-                <div className={styles.hugsSideBarLogo}>
-                  <Nav.Item className={styles.hugsSideNavBarTitle}></Nav.Item>
-                  {/* <Nav.Item className={styles.hugsSideNavBarShow}></Nav.Item> */}
-                </div>
-                <Nav.Link onClick={() => {handlePageChange("Coins")}} className={isCoins ? styles.hugsNavBarLink + " " + styles.overview + " " + styles.hugsNavBarLinkActive : styles.hugsNavBarLink + " " + styles.overview}>Coins</Nav.Link>
-                <Nav.Link onClick={() => {handlePageChange("Platforms")}} className={isPlatforms ? styles.hugsNavBarLink + " " + styles.platform + " " + styles.hugsNavBarLinkActive : styles.hugsNavBarLink + " " + styles.platform}>Platforms</Nav.Link>
+      <Container className={styles.mainPageContainer}>
+        <Container className={fullSidebar? styles.sidebarContainer: styles.sidebarContainer + " " + styles.showLessSidebar}>
+          <Navbar className={styles.fillHeight}>
+            <Nav className={styles.hugsSideNavBar}>
+              <div className={isCoins? styles.hugsSideBarLogo + " " + styles.BRB + " " + styles.BRT: styles.hugsSideBarLogo + " " + styles.BRT}>
+                <Nav.Item className={fullSidebar? styles.hugsSideNavBarTitle : styles.hugsSideNavBarTitle + " " + styles.hugsSideNavBarTitleHide}></Nav.Item>
+                {fullSidebar?
+                  <Nav.Item onClick={ShowLess} className={styles.hugsSideNavBarLess}></Nav.Item>
+                :
+                  <Nav.Item onClick={ShowMore} className={styles.hugsSideNavBarShow}></Nav.Item>
+                }
+              </div>
+              <Nav.Item className={isPlatforms? styles.hugsNavBarItem + " " + styles.BRB: styles.hugsNavBarItem}>
+                <Nav.Link onClick={() => {handlePageChange("Coins")}} className={isCoins ? 
+                  fullSidebar?
+                    styles.hugsNavBarLink + " " + styles.overview + " " + styles.hugsNavBarLinkActive :
+                    styles.hugsNavBarLink + " " + styles.overview + " " + styles.hugsNavBarLinkActiveHide
+                  : 
+                  styles.hugsNavBarLink + " " + styles.overview}>{fullSidebar? "Coins": ""}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item className={isCoins? styles.hugsNavBarItem + " " + styles.BRT + " " + styles.BRT: isDashboard? styles.hugsNavBarItem + " " + styles.BRB : styles.hugsNavBarItem}>
+                <Nav.Link onClick={() => {handlePageChange("Platforms")}} className={isPlatforms ? 
+                  fullSidebar?
+                    styles.hugsNavBarLink + " " + styles.platform + " " + styles.hugsNavBarLinkActive :
+                    styles.hugsNavBarLink + " " + styles.platform + " " + styles.hugsNavBarLinkActiveHide
+                    :
+                  styles.hugsNavBarLink + " " + styles.platform
+                  }>{fullSidebar? "Platforms": ""}</Nav.Link>
+              </Nav.Item>
+              <Nav.Item className={isPlatforms? styles.hugsNavBarItem + " " + styles.BRT: isProfiles? styles.hugsNavBarItem + " " + styles.BRB : styles.hugsNavBarItem}>
                 {isLogin?
-                  <Nav.Link onClick={() => {handlePageChange("Dashboard")}} className={isDashboard ? styles.hugsNavBarLink + " " + styles.dashboard + " " + styles.hugsNavBarLinkActive : styles.hugsNavBarLink + " " + styles.dashboard}>Dashboard</Nav.Link>
+                  <Nav.Link onClick={() => {handlePageChange("Dashboard")}} className={isDashboard ? 
+                    fullSidebar?
+                    styles.hugsNavBarLink + " " + styles.dashboard + " " + styles.hugsNavBarLinkActive :
+                    styles.hugsNavBarLink + " " + styles.dashboard + " " + styles.hugsNavBarLinkActiveHide
+                    :
+                    styles.hugsNavBarLink + " " + styles.dashboard
+                  }>{fullSidebar? "Dashboard": ""}</Nav.Link>
                 :
                   <></>
                 }
-                  {showMore?
-                  <Nav.Link onClick={() => {handlePageChange("Profiles")}} className={isProfiles ? styles.hugsNavBarLink + " " + styles.hugsNavBarLinkActive : styles.hugsNavBarLink}>Profiles</Nav.Link>
+              </Nav.Item>
+              <Nav.Item className={isDashboard? styles.hugsNavBarItem + " " + styles.BRT :styles.hugsNavBarItem}>
+                {showMore?
+                  <Nav.Link onClick={() => {handlePageChange("Profiles")}} className={isProfiles ? 
+                    fullSidebar?
+                    styles.hugsNavBarLink + " " + styles.dashboard + " " + styles.hugsNavBarLinkActive :
+                    styles.hugsNavBarLink + " " + styles.dashboard + " " + styles.hugsNavBarLinkActiveHide
+                    : styles.hugsNavBarLink + " " + styles.avatar 
+                  }>{fullSidebar? "Profiles": ""}</Nav.Link>
                 :
                   <></>
                 }
-                <img className={styles.hugsSideNavBarFooterGift} src='/static/src/gift.png'/>
-                <Nav.Item className={styles.hugsSideNavBarFooter}>
-                  <h1>Mint your NFT!</h1>
-                  <p>&#10004; Get full access</p>
-                  <p>&#10004; Earn Rewards</p>
-                  <p>&#10004; Gain Reputation.</p>
-                  <button className={styles.mainButton} type="button">Comming soon</button>
+              </Nav.Item>
+              <Nav.Item className={styles.hugsNavBarItemFill}></Nav.Item>
+              <Nav.Item className={isProfiles? styles.giftDiv + " " + styles.BRT : styles.giftDiv}>
+                {giftShow?
+                  <div className={styles.hugsSideNavBarFooterGiftDiv}>
+                    <img className={styles.hugsSideNavBarFooterGift} src='/static/src/gift.png'/>
+                    <Nav.Item className={styles.hugsSideNavBarFooter}>
+                      <h1>Mint your NFT!</h1>
+                      <p>&#10004; Get full access</p>
+                      <p>&#10004; Earn Rewards</p>
+                      <p>&#10004; Gain Reputation.</p>
+                      <button className={styles.mainButton} type="button">Comming soon</button>
+                    </Nav.Item>
+                  </div>
+                  :
+                  <div className={styles.hugsSideNavBarFooterGiftDiv}>
+                    <img className={styles.hugsSideNavBarFooterGiftSmall} src='/static/src/small-gift.svg'/>
+                    <a className={styles.hugsSideNavBarFooterGiftSmallLink}>Mint now</a>
+                  </div>
+                }
                 </Nav.Item>
-              </Nav>
-            </Container>
+            </Nav>
           </Navbar>
-          <Container className={styles.mainPageContainer + " " + styles.hugsSideNavContainer}>
-            <Navbar className={styles.hugsHeaderNavBar}>
-                <Nav.Link className={styles.hugsHeaderNavBarLink}>Overview</Nav.Link>
-                <Nav.Link className={styles.hugsHeaderNavBarLink}>How It works</Nav.Link>
-                <Nav.Link className={styles.hugsHeaderNavBarLink}>About Us</Nav.Link>
-                <Nav.Item className={styles.hugsHeaderNavBarLink}><ConnectButton handleOpenModal={onOpen}/></Nav.Item>
-                {userName?
-                  <Nav.Item className={styles.hugsHeaderNavBarItem}>{userName}</Nav.Item>
-                :
-                  <></>
-                }
-                {userAvatar?
-                  <Nav.Item className={styles.hugsHeaderNavBarItem}><img src={userAvatar}/></Nav.Item>
-                :
-                  <></>
-                }
-                
-            </Navbar>
-          </Container>
-          
-          <AccountModal isOpen={isOpen} onClose={onClose} />
-        </Layout>
-      </ChakraProvider>
-        <Container className={styles.mainPageContainer}>
+        </Container>
+        <Container className={fullSidebar? styles.mainContantContainer: styles.mainContantContainer + " " + styles.mainContantContainerMore }>
+          <ChakraProvider>
+            <Layout>
+              <Container className={styles.hugsSideNavContainer}>
+                <Navbar className={styles.hugsHeaderNavBar}>
+                    <Nav.Link className={styles.hugsHeaderNavBarLink}>Overview</Nav.Link>
+                    <Nav.Link className={styles.hugsHeaderNavBarLink}>How It works</Nav.Link>
+                    <Nav.Link className={styles.hugsHeaderNavBarLink}>About Us</Nav.Link>
+                    <Nav.Item className={styles.hugsHeaderNavBarLink}><ConnectButton handleOpenModal={onOpen}/></Nav.Item>
+                    {userName?
+                      <Nav.Item className={styles.hugsHeaderNavBarItem}>{userName}</Nav.Item>
+                    :
+                      <></>
+                    }
+                    {userAvatar?
+                      <Nav.Item className={styles.hugsHeaderNavBarItem}><img src={userAvatar}/></Nav.Item>
+                    :
+                      <></>
+                    }
+                </Navbar>
+              </Container>
+              <AccountModal isOpen={isOpen} onClose={onClose} />
+            </Layout>
+          </ChakraProvider>
           <section className={styles.cryptoList}>
             { isCoins ? (<CoinsList />) : (<></>) }
             { isDashboard ? (<Dashboard />) : (<></>) }
             { isPlatforms ? (<MarketList />) : (<></>) }
             { isProfiles ? (<ProfileList />): (<></>)}
           </section>
+        </Container>
+      </Container>
+      <Container>
         <Footer></Footer>
       </Container>
     </div>
