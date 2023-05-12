f3b0ed1db00657c28e86d4bd4aad73e1c0ec71b5
Mykyta Demianenko <mykytademianenko@gmail.com>
2023-05-12 15:15:23 +0300

    Append domain name for image media

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const nextConfig = {
 		NEXT_PUBLIC_SCAN_URL: process.env.NEXT_PUBLIC_SCAN_URL,
 		NEXT_PUBLIC_WALLET_PRIVATE_KEY: process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY,
 	},
-  reactStrictMode: true,
   images: {
     domains: [
-      'api.crtc.tech.assets.s3.eu-central-1.amazonaws.com'
+      'api.crtc.tech.assets.s3.eu-central-1.amazonaws.com',
+      'stage-earn-markets-assets.s3.eu-central-1.amazonaws.com',
+      'dev-earn-markets-assets.s3.eu-central-1.amazonaws.com'
     ]
   },
 }
