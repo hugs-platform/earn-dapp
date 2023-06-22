d5fa95e6b8c25dc8f6c836b509f8ad70a963bf8c
nickreznichuk <nickreznichuk@gmail.com>
2023-06-22 12:00:22 +0300

    [+] locale update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-		NEXT_PUBLIC_SCAN_URL: process.env.NEXT_PUBLIC_SCAN_URL,
-		NEXT_PUBLIC_WALLET_PRIVATE_KEY: process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY,
-	},
-  reactStrictMode: true,
-  images: {
-    domains: [
-      'dev-earn-markets-assets.s3.eu-central-1.amazonaws.com', 
-      'stage-earn-markets-assets.s3.eu-central-1.amazonaws.com',
-      'api.crtc.tech.assets.s3.eu-central-1.amazonaws.com',
-      'api.crtc.tech.assets.s3.eu-central-1.amazonaws.com',
-      'stage-earn-markets-assets.s3.eu-central-1.amazonaws.com',
-      'dev-earn-markets-assets.s3.eu-central-1.amazonaws.com'
-    ]
-  }
+    env: {
+        NEXT_PUBLIC_SCAN_URL: process.env.NEXT_PUBLIC_SCAN_URL,
+        NEXT_PUBLIC_WALLET_PRIVATE_KEY: process.env.NEXT_PUBLIC_WALLET_PRIVATE_KEY,
+    },
+    reactStrictMode: true,
+    i18n: {
+        locales: ["en-UK"],
+        defaultLocale: "en-UK"
+    },
+    images: {
+        domains: [
+            'dev-earn-markets-assets.s3.eu-central-1.amazonaws.com',
+            'stage-earn-markets-assets.s3.eu-central-1.amazonaws.com',
+            'api.crtc.tech.assets.s3.eu-central-1.amazonaws.com',
+            'api.crtc.tech.assets.s3.eu-central-1.amazonaws.com',
+            'stage-earn-markets-assets.s3.eu-central-1.amazonaws.com',
+            'dev-earn-markets-assets.s3.eu-central-1.amazonaws.com'
+        ]
+    }
 }
 
 module.exports = nextConfig
